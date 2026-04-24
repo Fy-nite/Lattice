ce0f61d9a672367d9ecdc009045d2216b13a01e0
charlie <gamerboy030607@gmail.com>
2026-04-25 00:27:24 +1000

    upload full.xml

---
+++ lattice.core/Entry.vb
@@ -0,0 +1,13 @@
+﻿
+Imports lattice.Runtime
+Imports ObjectIR.Core
+Imports ObjectIR.Core.Serialization
+Public Module Program
+    Public CPU As CPU
+    Public Sub Main(args As String())
+        Dim thing As ModuleData
+        Console.WriteLine(thing)
+        CPU = New CPU()
+        CPU.LoadProgram("demos/test.oir")
+    End Sub
+End Module
