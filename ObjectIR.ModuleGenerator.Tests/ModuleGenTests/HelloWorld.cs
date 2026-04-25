0f4bdd646d89f860f2b150699cd62ab97bbf625d
charlie <gamerboy030607@gmail.com>
2026-04-26 02:31:16 +1000

    upload tests

---
+++ ObjectIR.ModuleGenerator.Tests/ModuleGenTests/HelloWorld.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lattice.ObjectIR.Tests.NewFolder
+{
+    internal class HelloWorld
+    {
+        public void ConstructHelloWorld() { }
+    }
+}
