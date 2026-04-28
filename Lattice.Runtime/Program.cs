5682fa077d7744d942b9b2a00019541e32a996b6
Charlie <gamerboy030607@gmail.com>
2026-04-28 14:23:27 +1000

    update and introduce compiling like last time

--- Lattice.Runtime/Program.cs
+++ Lattice.Runtime/Program.cs
@@ -1,4 +1,6 @@
 ﻿using ObjectIR.Core;
+using ObjectIR.Core.AST;
+using ObjectIR.Core.Serialization;
 using ObjectIR.Stdlib.System;
 
 namespace lattice.Runtime;
@@ -6,25 +8,38 @@ using lattice;
 class Program
 {
     public static CPU cpu = new();
+    public static ModuleNode RootModule;
     public static bool DebugMode = false;
     public static string ProgramPath;
     static void Main(string[] args)
     {
         foreach (var arg in args)
         {
-            if (arg == "--debug")
-            {
-                DebugMode = true;
-            }
-            else
+            if (!arg.Contains("-"))
             {
                 ProgramPath = arg;
             }
+            else if (arg == "--debug")
+            {
+                DebugMode = true;
+            }
+            else if (arg == "--compile")
+            {
+                CompileModule = true;
+                RootModule = TextIrParser.ParseModule("");
+            }
+           
         }
 
         if (ProgramPath == null)
         {
-            IO.Println(new Value<Object>("Usage: lattice [--debug] <program.oir/fob>"));
+            IO.Println(new Value<Object>("Usage: lattice [--debug, --compile] <program.oir/fob>"));
+            Environment.Exit(1);
+        }
+
+        if (Path.GetExtension(ProgramPath).Contains(".oir"))
+        {
+            RootModule = ModuleSerializer.LoadFromBson(File.ReadAllBytes(ProgramPath));
         }
     }
 }
