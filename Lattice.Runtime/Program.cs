3d3b839cc1fe61b0646c5f6976109cd016305908
Charlie <gamerboy030607@gmail.com>
2026-04-28 11:37:22 +1000

    update lattice to use csharp for the entrypoint as visual basic apparently is being discontinued or something.
    the core hopefully will still have the visual basic stuff but all new CPU additions will be in csharp hopefully.

---
+++ Lattice.Runtime/Program.cs
@@ -0,0 +1,30 @@
+﻿using ObjectIR.Core;
+using ObjectIR.Stdlib.System;
+
+namespace lattice.Runtime;
+using lattice;
+class Program
+{
+    public static CPU cpu = new();
+    public static bool DebugMode = false;
+    public static string ProgramPath;
+    static void Main(string[] args)
+    {
+        foreach (var arg in args)
+        {
+            if (arg == "--debug")
+            {
+                DebugMode = true;
+            }
+            else
+            {
+                ProgramPath = arg;
+            }
+        }
+
+        if (ProgramPath == null)
+        {
+            IO.Println(new Value<Object>("Usage: lattice [--debug] <program.oir/fob>"));
+        }
+    }
+}
--- lattice.core/Entry.vb
+++ lattice.core/Entry.vb
@@ -1,54 +1,54 @@
-﻿
-Imports lattice.Runtime
-Imports ObjectIR.Core
-Imports ObjectIR.Core.Serialization
-Imports lattice.Throwables
-
-Public Module Program
-    Public CPU As CPU
-    Public Sub Main(args As String())
-        Try
-
-            Dim debugMode As Boolean = False
-            Dim programPath As String = Nothing
-            Dim Arguments As String() = {}
-
-            For i = 0 To args.Length - 1
-                Dim arg = args(i)
-
-                If arg = "--debug" Then
-                    debugMode = True
-
-                ElseIf arg = "--" Then
-                    Arguments = args.Skip(i + 1).ToArray()
-                    Exit For
-
-                ElseIf Not arg.StartsWith("-") AndAlso programPath Is Nothing Then
-                    programPath = arg
-                End If
-            Next
-
-
-            If programPath Is Nothing Then
-                Console.WriteLine("Usage: lattice [--debug] <program.oir>")
-                Environment.Exit(1)
-            End If
-
-            CPU = New CPU()
-            CPU.Debug = debugMode
-            CPU.LoadProgram(programPath)
-            CPU.Run(Arguments)
-        Catch ex As LatticeException
-            Console.ForegroundColor = ConsoleColor.Red
-            Console.Error.WriteLine(ex.Message)
-            Console.ResetColor()
-            Environment.Exit(1)
-        Catch ex As Exception
-            Console.ForegroundColor = ConsoleColor.DarkRed
-            Console.Error.WriteLine("An unexpected error occurred:")
-            Console.Error.WriteLine(ex.ToString())
-            Console.ResetColor()
-            Environment.Exit(1)
-        End Try
-    End Sub
-End Module
+﻿'
+'Imports lattice.Runtime
+'Imports ObjectIR.Core
+'Imports ObjectIR.Core.Serialization
+'Imports lattice.Throwables
+'
+'Public Module Program
+'    Public CPU As CPU
+'    Public Sub Main(args As String())
+'        Try
+'
+'            Dim debugMode As Boolean = False
+'            Dim programPath As String = Nothing
+'            Dim Arguments As String() = {}
+'
+'            For i = 0 To args.Length - 1
+'                Dim arg = args(i)
+'
+'                If arg = "--debug" Then
+'                    debugMode = True
+'
+'                ElseIf arg = "--" Then
+'                    Arguments = args.Skip(i + 1).ToArray()
+'                    Exit For
+'
+'                ElseIf Not arg.StartsWith("-") AndAlso programPath Is Nothing Then
+'                    programPath = arg
+'                End If
+'            Next
+'
+'
+'            If programPath Is Nothing Then
+'                Console.WriteLine("Usage: lattice [--debug] <program.oir>")
+'                Environment.Exit(1)
+'            End If
+'
+'            CPU = New CPU()
+'            CPU.Debug = debugMode
+'            CPU.LoadProgram(programPath)
+'            CPU.Run(Arguments)
+'        Catch ex As LatticeException
+'            Console.ForegroundColor = ConsoleColor.Red
+'            Console.Error.WriteLine(ex.Message)
+'            Console.ResetColor()
+'            Environment.Exit(1)
+'        Catch ex As Exception
+'            Console.ForegroundColor = ConsoleColor.DarkRed
+'            Console.Error.WriteLine("An unexpected error occurred:")
+'            Console.Error.WriteLine(ex.ToString())
+'            Console.ResetColor()
+'            Environment.Exit(1)
+'        End Try
+'    End Sub
+'End Module
